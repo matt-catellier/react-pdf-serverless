f7063400b608180cee63746678dd9b99e2989519
Matt Catellier <matt@fournine.digital>
2021-01-13 14:45:03 -0800

    Added example with cors for only some domains

--- src/index.js
+++ src/index.js
@@ -5,19 +5,32 @@ import streamToBuffer from './streamToBuffer';
 
 import MyDocument from './MyDocument'
 
+const ALLOWED_ORIGINS = [
+	'https://myfirstorigin.com',
+	'https://mysecondorigin.com'
+];
 
 const handler = async (event, ctx) => {
   const stream = await ReactPDF.renderToStream(<MyDocument />)
   
   const buffer = await streamToBuffer(stream)
   const imageBase64 = buffer.toString('base64')
+
+
+  const origin = event.headers.origin;
+  let headers;
+
+  if (ALLOWED_ORIGINS.includes(origin)) {
+    headers =  {
+      'Access-Control-Allow-Origin': origin,
+      'Access-Control-Allow-Credentials': true,
+    }
+  }  
+
+  headers['Content-Type'] = 'application/json'
   return {
       statusCode: 200,
-      headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true,
-      },
+      headers,
       body: JSON.stringify({
           imageBase64
       }),
